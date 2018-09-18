5589886b688c555eec01dff07c9bd71fa624bb34
Katnic <43383862+Katnic@users.noreply.github.com>
2018-09-18 13:33:03 -0300

    Add files via upload

---
+++ Encriptador/Encriptador.h
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Encriptador.h
+ * Author: lucas
+ *
+ * Created on 18 de septiembre de 2018, 13:14
+ */
+
+#ifndef ENCRIPTADOR_H
+#define	ENCRIPTADOR_H
+class Encriptador {
+public:
+	Encriptador();
+	Encriptador(const Encriptador& orig);
+	virtual ~Encriptador();
+private:
+
+};
+
+#endif	/* ENCRIPTADOR_H */
+
